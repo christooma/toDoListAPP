1e2f430a5a408c2e8713dc2c6e0a2bd7a62d8de5
fjordski <fordheacock@gmail.com>
2016-03-30 13:34:15 -0500

    Simple To-Do List App
    Currently it can add and delete tasks.  Fixing bug where it can’t
    delete most recently added tasks.
    
    Next up: clear search bar after search, strike through text when check
    box is clicked, add dates in bottom left of task, sort by date, add CSS
    transitions.

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+
+			//add input bar task to to-do list
+			$("#add-button").click(function () {
+				var task = $("#to-do").val();
+				console.log(task);
+				$("#incomplete-list").append("<li class='task'><h1 class='title'><input type='checkbox' name='checkbox'>" + task + "</h1><a class='delete' href='#'><p>delete</p></a></li>");
+
+			});
+
+			//removes task from list when 'delete' is clicked
+			$(".delete").click(function(){
+				$(this).closest('.task').remove();
+			});
+	
+
+
+
+
+			});
