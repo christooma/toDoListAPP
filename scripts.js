81f8ddc64b40f673c6c49d4564eee23dd19ef3d7
fjordski <fordheacock@gmail.com>
2016-03-31 13:25:01 -0500

    Can delete all to-do's now
    Made it so any ‘a’ link on click removes it’s parent.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
 
 
-			//add input bar task to to-do list
-			$("#add-button").click(function () {
-				var task = $("#to-do").val();
-				console.log(task);
-				$("#incomplete-list").append("<li class='task'><h1 class='title'><input type='checkbox' name='checkbox'>" + task + "</h1><a class='delete' href='#'><p>delete</p></a></li>");
+	//add input bar task to to-do list
+	$("#add-button").click(function () {
+		var task = $("#to-do").val();
+		console.log(task);
+		$("#incomplete-list").append("<li class='task'><h1 class='title'><input type='checkbox' name='checkbox'>" + task + "</h1><a class='delete' href='#'><p>delete</p></a></li>");
 
-			});
-
-			
-			//removes task from list when 'delete' is clicked
-			$(".delete").on("click", function(){
-				$(this).closest('li').remove();
-			});
-	
+	});
 
 
+	//removes task from list when 'delete' is clicked
+	$(document).on('click', 'a', function (e) {
+		e.preventDefault();
+		$(this).parent().remove();
+	});
 
 
-			});
+
+
+
+});
